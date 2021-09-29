ef7f5e78102ffc4e3dd73dd963e286daef8ff9d6
kilo718 <kilo718@naver.com>
2021-09-30 05:46:52 +0900

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,8 @@
 'use strict';
+import 'dotenv/config';
 
 module.exports = {
+  
   apps: [
     {
       name: "BootView", // pm2로 실행한 프로세스 목록에서 이 애플리케이션의 이름으로 지정될 문자열
@@ -8,14 +10,14 @@ module.exports = {
       watch: true, // 파일이 변경되면 자동으로 재실행 (true || false)
       env: {
         "NODE_ENV": "development", // 개발환경시 적용될 설정 지정
-        "TOKEN_SECRET": "CQ7ERGKXJ",
-        "TOKEN_SALT": "ROCKET",
-        "PATH_REFRESH_TOKEN": "REFRESH_HELLO123@",
-        "DB_HOST": "bootreview.cq7ergkxjze4.ap-northeast-2.rds.amazonaws.com",
-        "DB_USERNAME": "admin",
-        "DB_PASSWORD": "admin1346",
-        "DB_PORT": 3306,
-        "DB_DATABASE_NAME": "BootReview"
+        "TOKEN_SECRET": process.env.TOKEN_SECRET,
+        "TOKEN_SALT": process.env.TOKEN_SALT,
+        "PATH_REFRESH_TOKEN": process.env.PATH_REFRESH_TOKEN,
+        "DB_HOST": process.env.DB_HOST,
+        "DB_USERNAME": process.env.DB_USERNAME ,
+        "DB_PASSWORD": process.env.DB_PASSWORD,
+        "DB_PORT": process.env.DB_PORT,
+        "DB_DATABASE_NAME": process.env.DB_DATABASE_NAME
       },
       env_production: {
         "NODE_ENV": "production" // 배포환경시 적용될 설정 지정
