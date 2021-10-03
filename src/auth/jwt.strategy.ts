46402ec992306b4745f331f9fd53235994782a76
kilo718 <kilo718@naver.com>
2021-10-04 02:30:32 +0900

    fix/jwt_strategy

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -2,7 +2,7 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-
+import 'dotenv/config';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
@@ -11,7 +11,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration:true,
-      secretOrKey:'CQ7ERGKXJ'
+      secretOrKey:process.env.TOKEN_SECRET
     })
   }
   validate(payload: any) {
