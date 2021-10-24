8def49a3a7e3eb5f966057ca65d40139aa70f7bd
kilo718 <kilo718@naver.com>
2021-10-24 19:40:28 +0900

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as cookieparser  from 'cookie-parser'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000','https://bootview.info'],
+    origin: [,'https://bootview.info','http://localhost:3000'],
     methods: 'GET,PATCH,POST,OPTIONS',
     credentials: true,
     allowedHeaders: 'Content-Type, Authorization',
