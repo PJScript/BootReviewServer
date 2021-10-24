f4bafb50fb61618b717cc7c9e5e4c40af2eca565
kilo718 <kilo718@naver.com>
2021-10-24 14:16:54 +0900

    fix/socket.io cors

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -9,7 +9,7 @@ import { from, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Server } from 'socket.io';
 
-@WebSocketGateway(8888,{cors:{origin:'https://bootview.info'}
+@WebSocketGateway(8888,{cors:{origin:'*'}
 })
 export class ChatGateway {
   @WebSocketServer()
