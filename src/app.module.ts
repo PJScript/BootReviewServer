586b2735da868a94876e4c43d2c4d8fdbce3d154
감자 <gamja@gamjaui-MacBook-Air.local>
2022-05-03 23:16:47 +0900

    timezone fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -27,7 +27,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       type: 'mysql',
       host: configService.get('DB_HOST'),
       port: parseInt(configService.get('DB_PORT')),
-      timezone: "Asia/Seoul",
+      timezone: "+09:00",
       username: configService.get('DB_USERNAME'),
       password: configService.get('DB_PASSWORD'),
       database: configService.get('DB_DATABASE_NAME'),
